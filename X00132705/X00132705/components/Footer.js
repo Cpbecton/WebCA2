abb9b82621e6853b90de2428d8ffe2ecd2643344
Cpbecton <37289459+Cpbecton@users.noreply.github.com>
2018-12-10 13:03:52 +0000

    Add files via upload

---
+++ X00132705/X00132705/components/Footer.js
@@ -0,0 +1,21 @@
+// Footer component
+const Footer = () => (
+    <div>
+        <footer>
+            <p>Conor Becton: X00132705</p>
+        </footer>
+        <style jsx>{`
+            footer {
+                background-color: ;
+                max-width: 900px;
+                font-size: 0.8em;
+                text-align: center;
+                color: #00000;
+                clear: both;
+                margin: auto;
+            }
+        `}</style>
+    </div> 
+ )
+ 
+ export default Footer;
